6341e2ff29f87b53195ee6116e5f279afaf0b6d0
juanita mumbi <mumbijuanita@gmail.com>
2025-03-23 18:14:35 +0300

    functions lab complete

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-function saturdayFun(activity = "roller-skate") {
-    return This Saturday, I want to ${activity}!;
-}
-// saturdayFun();
-
-const mondayWork = function(activity = "go to the office") {
-    return This Monday, I will ${activity}.;
-};
-
-function wrapAdjective(wrapper = "*") {
+// saturdayFun
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // mondayWork
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // wrapAdjective
+  function wrapAdjective(flair = "*") {
     return function(adjective = "special") {
-        return You are ${wrapper}${adjective}${wrapper}!; 
+      return `You are ${flair}${adjective}${flair}!`;
     };
-}
+  }
